857eafd9a249515a5ad6da4eb155b01c88c48d4b
gunalansureshsm5598@gmail.com <gunalansm5598@gmail.com>
2024-09-23 18:56:46 +0530

    Updated sample code with proposaldat and liablity with address

--- expenditure.py
+++ expenditure.py
@@ -17,3 +17,63 @@
                     if category_name not in expenditure["SFS"]["FlexibleCosts"]:
                         expenditure["SFS"]["FlexibleCosts"][category_name] = {}
                     expenditure["SFS"]["FlexibleCosts"][category_name][field.replace("-", "")] = float(details["mentionText"])
+
+
+////secong try
+
+// Define Fixed and Flexible categories
+const fixedCategories = [
+    "expense-category-home-contents",
+    "expense-category-utilities",
+    "expense-category-water-supply",
+    "expense-category-care-health",
+    "expense-category-pension-insurances"
+];
+
+const flexibleCategories = [
+    "expense-category-food-housekeeping",
+    "expense-category-transport-travel",
+    "expense-category-communications-leisure",
+    "expense-category-personal"
+];
+
+// Initialize the resulting structure
+const expenditure = {
+    "Expenditure": {
+        "SFS": {
+            "FixedCosts": {},
+            "FlexibleCosts": {}
+        }
+    }
+};
+
+// Function to extract mentionText as number
+const extractValue = (item) => {
+    const key = Object.keys(item)[0];
+    return {
+        [key]: parseFloat(item[key].mentionText) // Convert mentionText to float
+    };
+};
+
+// Mapping FixedCosts
+fixedCategories.forEach(category => {
+    if (data[category]) {
+        expenditure.Expenditure.SFS.FixedCosts[category.replace("expense-category-", "").replace(/-/g, " ")] = {};
+        data[category][0].forEach(item => {
+            Object.assign(expenditure.Expenditure.SFS.FixedCosts[category.replace("expense-category-", "").replace(/-/g, " ")], extractValue(item));
+        });
+    }
+});
+
+// Mapping FlexibleCosts
+flexibleCategories.forEach(category => {
+    if (data[category]) {
+        expenditure.Expenditure.SFS.FlexibleCosts[category.replace("expense-category-", "").replace(/-/g, " ")] = {};
+        data[category][0].forEach(item => {
+            Object.assign(expenditure.Expenditure.SFS.FlexibleCosts[category.replace("expense-category-", "").replace(/-/g, " ")], extractValue(item));
+        });
+    }
+});
+
+// Resulting structure
+console.log(JSON.stringify(expenditure, null, 2));
